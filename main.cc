f8868d89741e20f4466b3b9c9283f907e3f282dc
IgnacioPeralta00 <00196321@uca.edu.sv>
2024-11-05 21:00:27 -0600

    added: New message

--- main.cc
+++ main.cc
@@ -4,5 +4,6 @@ int main(int argc, char const *argv[])
 {
     std::cout << "\nHola my name is Pablo and im gais\n";
     std::cout << "\nEsto es una prueba\n";
+    std::cout << "\nCashate la boca zorra\n";
     return 0;
 }
